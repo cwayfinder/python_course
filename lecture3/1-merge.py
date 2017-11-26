a7deb29154073c250645982fc13be338bc4981d5
Taras Hupalo <taras.hupalo@gmail.com>
2017-11-26 07:40:27 +0200

    refactor after review

--- lecture3/1-merge.py
+++ lecture3/1-merge.py
@@ -1,40 +1,16 @@
-def lines_count(file_name):
-    with open(file_name, 'rt') as f:
-        return sum(1 for line in f) - 1
-
-
 def merge_files(file1, file2):
-    length1 = lines_count(file1)
-    length2 = lines_count(file2)
-
-    with open(file1, 'rt') as source1, open(file2, 'rt') as source2, open('dest.txt', 'at') as dest:
-        dest.seek(0)
-        dest.truncate()
-
-        length = min(length1, length2)
-
-        i1 = 0
-        i2 = 0
-
+    with open(file1, 'rt') as source1, open(file2, 'rt') as source2, open('dest.txt', 'wt') as dest:
         line1 = source1.readline()
         line2 = source2.readline()
-        while i1 + i2 < length * 2:
-            if line1 <= line2:
-                dest.write(line1)
-                i1 += 1
+        while line1 or line2:
+            num1 = int(line1) if line1 else float('inf')
+            num2 = int(line2) if line2 else float('inf')
+            if num1 < num2:
+                dest.write('{}\n'.format(num1))
                 line1 = source1.readline()
             else:
-                dest.write(line2)
-                i2 += 1
+                dest.write('{}\n'.format(num2))
                 line2 = source2.readline()
-
-        while i1 < length1:
-            dest.write(source1.readline())
-            i1 += 1
-
-        while i2 < length2:
-            dest.write(source2.readline())
-            i2 += 1
 
 
 merge_files('sorted1.txt', 'sorted2.txt')
