85aab95fd049ffb1c921177db4c0409edccf2a52
Taras Hupalo <taras.hupalo@gmail.com>
2017-12-18 21:16:46 +0200

    lecture 7 tasks

---
+++ lecture13/main.py
@@ -0,0 +1,36 @@
+import datetime
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name='Anonymous'):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    message = date = task = ''
+    if request.method == 'POST':
+        date = request.form.get('date', '')
+        task = request.form.get('task', '')
+        print(date, task)
+        try:
+            if not date or task:
+                raise ValueError
+            date = datetime.datetime.strptime(date, '%s.%s.%s')
+        except ValueError:
+            message = 'Incorrect input'
+    return render_template('add.html', date=date, task=task, message=message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
